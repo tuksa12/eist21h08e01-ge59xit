cac850233cc8951beeab39d043171de7bbcd91bd
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2021-06-27 16:26:13 +0200

    Test 1

--- test/de/tum/in/ase/eist/CourseTest.java
+++ test/de/tum/in/ase/eist/CourseTest.java
@@ -1,9 +1,27 @@
 package de.tum.in.ase.eist;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
 public class CourseTest {
 
 	// TODO 1: Test getCourseTitle()
+    @Test
+    public void testGetCourseTitle() {
+        Course testCourse = new Course("Title");
+        assertEquals(testCourse.getCourseTitle(),"Title");
+    }
 
 	// TODO 2: Test getNumberOfAttendees()
+    @Test
+    public void testNoAttendees() {
+        Course testCourse = new Course("Title");
+        assertEquals(testCourse.getAttendees(), 0);
+    }
 
+    @Test
+    public void testThreeAttendees() {
+        Course testCourse = new Course("Title");
+        assertEquals(testCourse.getAttendees(), 3);
+    }
 }
--- test/de/tum/in/ase/eist/OnlineCourseTest.java
+++ test/de/tum/in/ase/eist/OnlineCourseTest.java
@@ -1,6 +1,33 @@
 package de.tum.in.ase.eist;
 
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
 public class OnlineCourseTest {
 
 	// TODO 3: Test setOnlineCourseUrl()
+    @Test
+    public void testSetOnlineCourseUrlWithValidUrl() {
+        OnlineCourse testOnline = new OnlineCourse("Title");
+        try {
+            URL url = new URL("http://www.example.com/docs/resource1.html");
+            testOnline.setOnlineCourseUrl("http://www.example.com/docs/resource1.html");
+            assertEquals(testOnline.getOnlineCourseUrl(),url);
+        } catch (Exception ignored) {
+
+        }
+    }
+
+    @Test
+    public void testSetOnlineCourseUrlWithInvalidUrl() {
+        OnlineCourse testOnline = new OnlineCourse("Title");
+        try {
+            testOnline.setOnlineCourseUrl("ocs/resource1.html");
+        } catch (Exception ignored) {
+            assertEquals(1,1);
+        }
+    }
 }
